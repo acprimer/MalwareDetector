7b57d42405de5f7d5a4b0872169f7988d5ed3c11
acprimer <729101421@qq.com>
2015-05-19 15:09:37 +0800

    smali

---
+++ app/src/main/java/org/jf/baksmali/Adaptors/Constants.java
@@ -0,0 +1,13 @@
+package org.jf.baksmali.Adaptors;
+
+public class Constants {
+
+	public static final int ALL = 1;
+    public static final int ALLPRE = 2;
+    public static final int ALLPOST = 4;
+    public static final int ARGS = 8;
+    public static final int DEST = 16;
+    public static final int MERGE = 32;
+    public static final int FULLMERGE = 64;
+
+}
